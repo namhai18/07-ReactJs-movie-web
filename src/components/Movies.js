1bba03d964465efa1b43a89787ef0fad984ac7ce
namhai18 <namhai18@gmail.com>
2020-03-23 07:21:36 +0700

    1. nhận data về từ redux về biến bóc tách 2. hiển thị data lên từng thẻ Card Movie

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Movies extends Component {
+    
     render() {
+        const {movie} = this.props;
+
         return (
             <div className="col-sm-3">
                 <div className="card text-left">
-                    <img className="card-img-top" src="holder.js/100px180/" alt=""/>
+                    <img className="card-img-top" src={movie.hinhAnh} alt=""/>
                     <div className="card-body">
-                        <h4 className="card-title">Title</h4>
-                        <p className="card-text">Body</p>
+                        <h4 className="card-title">{movie.tenPhim}</h4>
+                        <p className="card-text">{movie.biDanh}</p>
+                        
                     </div>
                 </div>
             </div>
