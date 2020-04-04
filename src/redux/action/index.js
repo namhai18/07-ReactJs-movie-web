d7f43ff5a650a5ebbaf203a5a71ae4fee3ae1f5c
namhai18 <nguyen.tran.nam.hai@geonet.co.jp>
2020-04-04 13:49:44 +0700

    1. Gọi API . 2. Store giá trị lên redux.

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,20 +1,42 @@
 import * as ActionType from "./../constant/ActionType"
 import Axios from "axios";
 
+export const actLoginAdminAPI = (user, history) => {
+  return dispatch => {
+    Axios({
+      method: "POST",
+      url: "http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap",
+      data: user
+    }).then(rs => {
+      console.log("actLoginAdminAPI");
+      if (rs.data.maLoaiNguoiDung === "QuanTri") {
+        // save user da log in xuong localStorage (có token)
+        localStorage.setItem("userAdmin",JSON.stringify(rs.data));
+        // Dùng history để chuyển hướng trang đến trang tiếp theo
+        history.push("/admin/dashboard")
+      }else {
+        alert("Không có quyền");
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+}
+
 export const actGetDetailMovieAPI = (id) => {
   return dispatch => {
     // http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=1314
     Axios({
       method: "GET",
       url: `http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=${id}`
-    }).then (rs =>{
+    }).then(rs => {
       console.log("actGetDetailMovieAPI");
       // send data get ve tu axios to redux
       dispatch({
         type: ActionType.GET_DETAIL_MOVIE,
         data: rs.data
       });
-    }).catch(err =>{
+    }).catch(err => {
       console.log(err);
     });
   }
@@ -25,22 +47,22 @@ export const actGetListMovieAPI = () => {
     Axios({
       method: "GET",
       url: "http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01"
-    }).then (rs =>{
+    }).then(rs => {
       console.log(rs.data);
       // send data thang len store sau khi API tra ve data
       dispatch(getListMovies(rs.data));
-    }).catch(err =>{
+    }).catch(err => {
       console.log(err);
     });
   };
 };
 
 export const getListMovies = (listMovie) => {
-    return {
-            type: ActionType.GET_LIST_MOVIE,
-            data: listMovie
-          }
-    };
+  return {
+    type: ActionType.GET_LIST_MOVIE,
+    data: listMovie
+  }
+};
 
     // export const actGetDetailMovie = () => {
     //   return {
