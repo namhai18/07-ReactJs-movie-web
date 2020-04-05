3962b55d51fa11446450a612cb4a8e10f568211e
namhai18 <nguyen.tran.nam.hai@geonet.co.jp>
2020-04-05 13:44:51 +0700

    1. add them action them nguoi dung tren redux (co kèm token verify vào header) 2.  thêm form input AddNewUser , event onChange , event Submit , send to action -> action send to DB qua API.

--- src/pages/admin/ThemNguoiDung.js
+++ src/pages/admin/ThemNguoiDung.js
@@ -1,11 +1,91 @@
 import React, { Component } from 'react'
+import * as action from "./../../redux/action";
+import { connect } from "react-redux";
 
-export default class ThemNguoiDung extends Component {
+class ThemNguoiDung extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            taiKhoan: "",
+            matKhau: "",
+            hoTen: "",
+            email: "",
+            soDt: "",
+            maNhom: "",
+            maLoaiNguoiDung: "",
+        }
+    }
+
+    // Khi input thay đổi value thì set lại state
+    handleOnChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            // copy lai state value , va multi-form gan lai 
+            [name]: value 
+        },
+            () => {
+                // console.log(this.state);
+            });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state);
+        // truyền vào cục user chính là this.state.
+        this.props.addNewUser(this.state);
+    }
+
     render() {
         return (
             <div>
-                ThemNguoiDung
+                <form className="container" onSubmit={this.handleSubmit}> 
+                    <h3>Thêm người dùng</h3>
+                    <div className="form-group">
+                        <span>Tài khoản</span>
+                        <input className="form-control" name="taiKhoan" onChange={this.handleOnChange} />
+                    </div>
+                    <div className="form-group">
+                        <span>Mật khẩu</span>
+                        <input className="form-control" name="matKhau" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <span>Họ tên</span>
+                        <input className="form-control" name="hoTen" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <span>Email</span>
+                        <input className="form-control" name="email" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <span>Số điện thoại</span>
+                        <input className="form-control" name="soDt" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <span>Mã nhóm</span>
+                        <input className="form-control" name="maNhom" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <span>Mã loại người dùng</span>
+                        <input className="form-control" name="maLoaiNguoiDung" onChange={this.handleOnChange}/>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-success">Thêm người dùng</button>
+                    </div>
+                </form>
             </div>
         )
     }
 }
+
+// map to store
+const mapDispatchToProps = dispatch => {
+    return {
+        // key(addNewUser) cũng chính là props
+        addNewUser: (user) => {
+            dispatch(action.actAddNewUserAPI(user));
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ThemNguoiDung);
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,6 +1,37 @@
 import * as ActionType from "./../constant/ActionType"
 import Axios from "axios";
 
+// http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThemNguoiDung
+export const actAddNewUserAPI = (user) => {
+  // Xuống local storage collect lên lại parse ra JSON
+  const userLocalStorage = JSON.parse(localStorage.getItem("userAdmin"))
+
+  return dispatch => {
+    Axios({
+      method: "POST",
+      url: "http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThemNguoiDung",
+      data: user,
+      // add token vao headers
+      headers: {
+        Authorization: "Bearer " + userLocalStorage.accessToken
+      }
+    }).then(rs => {
+      console.log("actAddNewUserAPI");
+
+      // if (rs.data.maLoaiNguoiDung === "QuanTri") {
+      //   // save user da log in xuong localStorage (có token)
+      //   localStorage.setItem("userAdmin",JSON.stringify(rs.data));
+      //   // Dùng history để chuyển hướng trang đến trang tiếp theo
+      //   history.push("/admin/dashboard")
+      // }else {
+      //   alert("Không có quyền");
+      // }
+    }).catch(err => {
+      console.log(err.response.data);
+    });
+  }
+}
+
 export const actLoginAdminAPI = (user, history) => {
   return dispatch => {
     Axios({
@@ -11,10 +42,10 @@ export const actLoginAdminAPI = (user, history) => {
       console.log("actLoginAdminAPI");
       if (rs.data.maLoaiNguoiDung === "QuanTri") {
         // save user da log in xuong localStorage (có token)
-        localStorage.setItem("userAdmin",JSON.stringify(rs.data));
+        localStorage.setItem("userAdmin", JSON.stringify(rs.data));
         // Dùng history để chuyển hướng trang đến trang tiếp theo
         history.push("/admin/dashboard")
-      }else {
+      } else {
         alert("Không có quyền");
       }
     }).catch(err => {
@@ -48,6 +79,7 @@ export const actGetListMovieAPI = () => {
       method: "GET",
       url: "http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01"
     }).then(rs => {
+      console.log("actGetListMovieAPI");
       console.log(rs.data);
       // send data thang len store sau khi API tra ve data
       dispatch(getListMovies(rs.data));
@@ -64,9 +96,9 @@ export const getListMovies = (listMovie) => {
   }
 };
 
-    // export const actGetDetailMovie = () => {
-    //   return {
-    //     type: ActionType.GET_LIST_MOVIE,
-    //     data: listMovie
-    //   }
-    // };
+export const actGetDetailMovie = (detailMovie) => {
+  return {
+    type: ActionType.GET_DETAIL_MOVIE,
+    data: detailMovie
+  }
+};
